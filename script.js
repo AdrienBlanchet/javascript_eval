0eb630475dc81c650dc84b3a58b8e35ecd1a7b71
Adrien Blanchet <adrien131313@gmail.com>
2021-11-20 02:02:20 +0100

    html/css: presque fini / javascript: addeventlistener sur click bouton ok

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-const button = document.querySelector('.button')
-const player1 = document.querySelector('.player1')
-
 const tab = [1, 2, 3, 4, 5, 6];
 
 //Fonction tirage aléatoire
 let play = () => Math.round(Math.random() * tab.length);
 //////////////////////////
 
-button.addEventListener('click', () => {
 
-    player1.innerHTML  = play();
+const buttons = document.querySelectorAll('.btn');
 
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) =>{
+        
+        switch (e.target.innerHTML) {
+            case "New Game":
+              alert('new');
+              break;
+            case "Roll Dice":
+              alert('roll');
+              break;
+            case "Hold":
+              alert('hold');
+              break;
+            default:
+              null;
+          }
+    })
 })
+    
 
-console.log(button);
 
