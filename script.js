d53949eea0bfead5e979167d44e7a3acf959a508
Adrien Blanchet <adrien131313@gmail.com>
2021-11-21 01:11:49 +0100

    logique affichage du lancer ok, enregistrement du score global en cours

--- script.js
+++ script.js
@@ -1,11 +1,66 @@
+const buttons = document.querySelectorAll('.btn');
+const icon = document.querySelector('.des')
+const scoreRound1 = document.querySelector('.scoreRound1')
+const scoreGlobal1 = document.querySelector('.scoreGlobal1')
 const tab = [1, 2, 3, 4, 5, 6];
+let result = 0;
+let somme = 0;
+console.log(scoreRound1);
 
 //Fonction tirage aléatoire
-let play = () => Math.round(Math.random() * tab.length);
-//////////////////////////
 
 
-const buttons = document.querySelectorAll('.btn');
+let play = () => {
+ result =  Math.floor(Math.random() * tab.length+1);
+ //console.log(result); 
+      switch (result) {
+        case 1:
+          icon.setAttribute('src', 'images/one.png'); 
+          score1(result);
+          alert('tour suivant');
+          break;
+        case 2:
+          icon.setAttribute('src', 'images/two.png'); 
+          score1(result);
+          break;
+        case 3:
+          icon.setAttribute('src', 'images/three.png');
+          score1(result);
+          break;
+        case 4:
+          icon.setAttribute('src', 'images/four.png');
+          score1(result);
+          break;
+        case 5:
+          icon.setAttribute('src', 'images/five.png'); 
+          score1(result);
+          break;
+        case 6:
+          icon.setAttribute('src', 'images/six.png'); 
+          score1(result);
+          break;
+        default:
+          null;
+      }
+};
+
+
+let score1 = (res) => {
+  
+  if(res == 1){
+    scoreRound1.innerHTML = 0; 
+    
+  }else{
+    somme += res  
+    scoreRound1.innerHTML = somme;
+  }
+}
+
+let scoreSave = (somme) => {
+scoreGlobal1.innerHTML = somme;
+}
+
+
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) =>{
@@ -15,10 +70,10 @@ buttons.forEach((button) => {
               alert('new');
               break;
             case "Roll Dice":
-              alert('roll');
+              play()
               break;
             case "Hold":
-              alert('hold');
+              scoreSave(somme);
               break;
             default:
               null;
